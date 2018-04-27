5d15ca277c48aa9d98a01383b3941ce11dfe73b5
Oscar Villalta <bagglerock@gmail.com>
2018-04-27 15:51:43 -0400

    have the logic working... have to the page together

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,46 +2,58 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
 
+  function checkDupe(person) {
+    var dupe = false;
+    for (var i = 0; i< friends.length; i++){
+      if (person.name === friends[i].name && person.photo === friends[i].photo){
+          dupe = true;
+      }
+    }
+    return dupe;
+  
+  }
+
+  function findMatch(person) {
+    var mostCompatible;
+    var mostCompatibleScore;
+
+    for(var i = 0; i < friends.length; i++){
+      var compatibilityComparison = 0;
+      if(person.name !== friends[i].name && person.photo !== friends[i].photo){
+        for (var j = 0; j < person.scores.length; j++){
+          compatibilityComparison += Math.abs(parseInt(person.scores[j]) - parseInt(friends[i].scores[j]));
+        } 
+        if (mostCompatibleScore && compatibilityComparison < mostCompatibleScore){
+            mostCompatible = friends[i].name;
+            mostCompatibleScore = compatibilityComparison;
+        } else {
+          mostCompatible = friends[i].name;
+          mostCompatibleScore = compatibilityComparison;
+        }
+      }
+
+    }
+    return mostCompatible;
+
+  }
+
+
+
   // Get ALL friends
   app.get('/api/friends', function(req, res) {
-  
     return res.json(friends);
-
   });
 
   app.post('/api/friends', function(req, res) {
-    //add compatibility logic here
+
     var newFriend = req.body;
-    var newFriendName = newFriend.name;
-    var newFriendPhoto = newFriend.photo;
-    var newFriendScore = newFriend.scores;
-    //boolean or something to hold the name of the most compatible and the score of the most compatible
-    var mostCompatible = "";
-    var mostCompatibleScore = "";
 
-    for (var i = 0; i < friends.length ; i++){
-      var name = friends[i].name;
-      var photo = friends[i].photo;
-      var score = friends[i].scores;
-      compatibilityScore = 100;
-      if(newFriendName !== name && newFriendPhoto !== photo){
-        for (var j = 0; j < score.length; j++){
-          //compare the scores using Math.abs(int);
-          compatibilityScore += Math.abs(parseInt(newFriendScore[j]) - parseInt(score[j]));
-          if(compatibilityScore < mostCompatibleScore){
-            mostCompatible = name;
-            mostCompatibleScore = compatibilityScore;
-          }
-        }
-
-      }
-      console.log("Most compatible match is " + mostCompatible);
-    }
+    var match = findMatch(newFriend);
 
     friends.push(newFriend);
+    console.log("Most compatible match is " + match);
     res.json(newFriend);
 
   });
-
 
 };
