ca8c04b68a01aa93a235eb9e99e4543d28119365
Oscar Villalta <bagglerock@gmail.com>
2018-04-27 00:17:29 -0400

    tired and the logic is not quite there yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,14 +9,36 @@ module.exports = function(app) {
 
   });
 
-  app.post('/api/friends/new', function(req, res) {
-
+  app.post('/api/friends', function(req, res) {
+    //add compatibility logic here
     var newFriend = req.body;
+    var newFriendName = newFriend.name;
+    var newFriendPhoto = newFriend.photo;
+    var newFriendScore = newFriend.scores;
+    //boolean or something to hold the name of the most compatible and the score of the most compatible
+    var mostCompatible = "";
+    var mostCompatibleScore = "";
 
-    console.log(newFriend);
+    for (var i = 0; i < friends.length ; i++){
+      var name = friends[i].name;
+      var photo = friends[i].photo;
+      var score = friends[i].scores;
+      compatibilityScore = 100;
+      if(newFriendName !== name && newFriendPhoto !== photo){
+        for (var j = 0; j < score.length; j++){
+          //compare the scores using Math.abs(int);
+          compatibilityScore += Math.abs(parseInt(newFriendScore[j]) - parseInt(score[j]));
+          if(compatibilityScore < mostCompatibleScore){
+            mostCompatible = name;
+            mostCompatibleScore = compatibilityScore;
+          }
+        }
+
+      }
+      console.log("Most compatible match is " + mostCompatible);
+    }
 
     friends.push(newFriend);
-
     res.json(newFriend);
 
   });
